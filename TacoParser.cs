85ca43d056888ac5ee41658cc659864b691f246c
Michelle Stevens <stevens.michellec@gmail.com>
2021-05-03 19:47:13 -0700

    TacoPreject

---
+++ TacoParser.cs
@@ -0,0 +1,56 @@
+﻿namespace LoggingKata
+{
+    /// <summary>
+    /// Parses a POI file to locate all the Taco Bells
+    /// </summary>
+    public class TacoParser
+    {
+        readonly ILog logger = new TacoLogger();
+        
+        public ITrackable Parse(string line)
+        {
+            //logger.LogInfo("Begin parsing");
+
+            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            var cells = line.Split(',');
+
+            // If your array.Length is less than 3, something went wrong
+            if (cells.Length < 3)
+            {
+                // Log that and return null
+                // Do not fail if one record parsing fails, return null
+                return null; // TODO Implement
+            }
+
+            // grab the latitude from your array at index 0
+            var lat = double.Parse(cells[0]);
+            // grab the longitude from your array at index 1
+            var lon = double.Parse(cells[1]);
+            // grab the name from your array at index 2
+            var tacoBellName = cells[2];
+
+            // Your going to need to parse your string as a `double`
+            // which is similar to parsing a string as an `int` DONE
+
+            // You'll need to create a TacoBell class
+            // that conforms to ITrackable DONE
+
+
+            // Then, you'll need an instance of the TacoBell class
+            // With the name and point set correctly
+            var point = new Point();
+            point.Latitude = lat;
+            point.Longitude = lon;
+
+
+            // Then, return the instance of your TacoBell class
+            // Since it conforms to ITrackable
+            var tacoBell = new TacoBell();
+            tacoBell.Name = tacoBellName;
+            tacoBell.Location = point;
+
+
+            return tacoBell;
+        }
+    }
+}
